541e10badfe566c55b48beaf9679f67f62c6a4d7
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2022-09-08 17:31:42 +0800

    更新 publish.sh

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ yarn build
 mkdir pages
 cd pages
 git init
-git config --global user.name "sjfkai"
-git config --global user.email "sjfkai@163.com"
+git config --global user.name "番茄酱"
+git config --global user.email "aidengrong@aidengrong.top"
 git remote add github $GIT_REPO
 git fetch github
 git merge github/gh-pages
