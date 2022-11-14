693b81cb7357b054c226b5a3ce7b80829453b00e
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2022-11-14 16:42:24 +0800

    更新 download.js

--- src/utils/download.js
+++ src/utils/download.js
@@ -4,8 +4,8 @@ export async function downloadExcel (data) {
   const XLSX = await import(/* webpackChunkName: "xlsx" */ 'xlsx')
   const workbook = XLSX.utils.book_new()
   const worksheet = XLSX.utils.aoa_to_sheet(data)
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'mapLocation.sjfkai.com')
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'map.aidengrong.top')
   const wopts = { bookType:'xlsx', bookSST:false, type:'array' }
   const wbout = XLSX.write(workbook,wopts)
-  saveAs(new Blob([wbout],{type: 'application/octet-stream'}), 'map-location.xlsx')
+  saveAs(new Blob([wbout],{type: 'application/octet-stream'}), '番茄map.xlsx')
 }
