607fd9993f882635d3c4d48fdb412a0aaff3e33f
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2025-09-05 10:46:42 +0800

    更新 App.js

--- src/App.js
+++ src/App.js
@@ -225,8 +225,9 @@ class App extends Component {
           <Footer className="footer">
             <div className="footer-link">
               <a href="https://blog.1314151.xyz" target="_blank"  rel="noopener noreferrer">番茄酱的blog</a>
+             <a href="https://web.1314151.xyz" target="_blank"  rel="noopener noreferrer">番茄酱的导航</a>
+             <a href="https://wallpaper.1314151.xyz" target="_blank"  rel="noopener noreferrer">番茄酱的壁纸</a>
             </div>
-             <a href="https://blog.1314151.xyz" target="_blank"  rel="noopener noreferrer">番茄酱的blog</a>
             <p>Developed by <a href="mailto:aidengrong@gmail.com" target="_top" className="bold">aidengrong@gmail.com</a></p>
             {/* <p>Hosted by <a href="https://pages.coding.me" className="bold">Coding Pages</a></p> */}
           </Footer>
